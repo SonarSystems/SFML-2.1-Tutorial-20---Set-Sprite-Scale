6309ba90bd50e75136edc0a66d7d43955a702abe
Sonar Systems <admin@sonarsystems.co.uk>
2014-08-16 10:34:29 +0100

    Set Sprite Scale

---
+++ TutorialSFML/TutorialSFML/main.cpp
@@ -0,0 +1,48 @@
+#include "SFML/Graphics.hpp"
+#include <iostream>
+
+int main()
+{
+	sf::RenderWindow window( sf::VideoMode( 600, 600 ), "SFML WORK!" );
+
+	sf::Texture texture;
+
+	if (!texture.loadFromFile("Paddle Large.png"))
+	{
+		std::cout << "Error loading paddle texture :(" << std::endl;
+	}
+
+	//sf::Sprite sprite(texture);
+
+	// or
+
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+
+	sprite.setScale(sf::Vector2f(0.5, 4));
+
+	std::cout << sprite.getScale().x << std::endl;
+
+	while ( window.isOpen( ) )
+	{
+		sf::Event event;
+
+		while ( window.pollEvent( event ) )
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+
+				break;
+
+			}
+		}
+
+		window.clear( );
+
+		window.draw(sprite);
+
+		window.display( );
+	}
+}
